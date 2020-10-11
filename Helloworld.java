012d2ed54f6e8b5a5592d5dacd0cbf13b1c5c1b4
hbn <hoang.bach.nguyen@ppi.de>
2020-10-11 23:57:17 +0200

    Add Common-Class

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
 public class Helloworld{
 	public static void main (String[] args){
-	System.out.println("Hello world!");	
+	System.out.println("Hello world!");
+	System.out.println("Sum is: "+Common.Sum(5,7));
 	}
 }
