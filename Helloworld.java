4f61e70e4ca18d21ec8ee826495f16c976317807
hbn <hoang.bach.nguyen@ppi.de>
2020-10-12 00:23:39 +0200

    Sum first, then print

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,7 @@
 public class Helloworld{
 	public static void main (String[] args){
 	System.out.println("Hello world!");
-	System.out.println("Sum is: "+Common.Sum(5,7));
+	int Sum = Common.Sum(5,7);
+	System.out.println("Sum is: "+Sum);
 	}
 }
