0771534064c8065aad6f90ba1a9fc78d5d05cf85
hbn <hoang.bach.nguyen@ppi.de>
2020-10-12 15:42:47 +0200

    Add Convert 1252 to UTF8

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,16 @@
 public class Helloworld{
+
+  public String convert1252toUTF8(String inputText1252){
+    final Charset fromCharset = Charset.forName("windows-1252");
+    final Charset toCharset = Charset.forName("UTF-8");
+    String outputTextUTF8 = String(inputText1252.getBytes(fromCharset),toCharset);
+    return outputTextUTF8;
+  }
+
 	public static void main (String[] args){
 	System.out.println("Hello world!");
 	int Sum = Common.Sum(5,7);
 	System.out.println("Sum is: "+Sum);
+	System.out.println("Müller 1252 --> UTF8: "+convert1252toUTF8 ("Müller"));
 	}
 }
